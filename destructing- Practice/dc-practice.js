e0943cff7d4d154e217229e996ec0f9ee9cdeca2
miaozy9 <1@miaozy.com>
2020-12-12 01:31:54 -0500

    upload destructing practice

---
+++ destructing- Practice/dc-practice.js
@@ -0,0 +1,32 @@
+//8
+//1846
+
+//{yearNeptuneDiscovered: 1846, yearMarsDiscovered: 1659}
+
+// Your name is Alejandro and you like purple
+// Your name is Melissa and you like green
+// Your name is undefined and you like green
+
+
+// Maya
+// Marisa
+// Chi
+
+// "Raindrops on roses"
+// "whiskers on kittens"
+// ["Bright copper kettles", "warm woolen mittens", "Brown paper packages tied up with strings"]
+
+// [10,30,20]
+
+const obj = {
+    numbers: {
+      a: 1,
+      b: 2
+    }
+  };
+  
+const {a,b} = obj.numbers
+
+[arr[0], arr[1]] = [arr[1], arr[0]]
+
+const raceResults = ([first, second, third, ...rest]) => ({first, second, third, rest})

46e3d1ba9ef4b821814548bdbf43f9b9b9ab13d5
miaozy9 <1@miaozy.com>
2020-12-25 22:32:50 -0500

    ajax api

--- ajax-giphy-party/app.js
+++ ajax-giphy-party/app.js
@@ -1,1 +1,31 @@
-console.log("Let's get this party started!");
+// console.log("Let's get this party started!");
+
+
+$("#searchForm").on("submit", async function(e){
+    e.preventDefault()
+    let term = $("#searchTerm").val()
+    const response = await axios.get("http://api.giphy.com/v1/gifs/search",{params:{
+        q : term,
+        api_key : "p5HC15nFkERXk9nMCe8eNROnaTxGw3w8"
+    }
+    })
+
+
+
+    let numResults = response.data.data.length
+    console.log(numResults)
+    let random = Math.floor(Math.random() * numResults)
+
+    if(numResults){
+        
+        let img = $("<img>",{
+            src : response.data.data[random].images.original.url
+        })
+        $("#gif").append(img)
+    }
+})
+
+$("#remove").on("click", function() {
+    $("#gif").empty();
+  });
+
